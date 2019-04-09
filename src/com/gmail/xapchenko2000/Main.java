18c8748eb7fb5a9c3d124f4498abb68159ebca01
Vlad Kharchenko <xapchenko2000@gmail.com>
2019-04-09 12:56:19 +0300

    First print

---
+++ src/com/gmail/xapchenko2000/Main.java
@@ -0,0 +1,46 @@
+package com.gmail.xapchenko2000;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static String name;
+    static String city;
+    static int age;
+    static String hobby;
+
+    public static void main(String[] args) {
+
+        enterData();
+        printOne();
+    }
+
+
+    public static void enterData() {
+
+
+        Scanner csOne = new Scanner(System.in);
+        Scanner csHobby = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        name = csOne.nextLine();
+
+        System.out.println("Enter your city where you were born: ");
+        city = csOne.nextLine();
+
+        System.out.println("Enter your age: ");
+        age = csOne.nextInt();
+
+        System.out.println("Enter your hobby: ");
+        hobby = csHobby.nextLine();
+
+    }
+
+    public static void printOne() {
+        System.out.println("Имя " + "    " + "\t" + name);
+        System.out.println("Возраст " + "\t" + age);
+        System.out.println("Город " + "  " + "\t" + city);
+        System.out.println("Хобби " + "  " + "\t" + hobby);
+
+    }
+}
